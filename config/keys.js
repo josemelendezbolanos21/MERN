b3a37807223194f5f375d088ef4fbe3e2bc22be5
josemelendezbolanos21 <jose.melendezbolanos@gmail.com>
2018-07-08 13:13:54 -0400

    Added login and token authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://jose:jose123@ds035573.mlab.com:35573/dev_connector_course'
+  mongoURI: 'mongodb://jose:jose123@ds035573.mlab.com:35573/dev_connector_course',
+  secretOrKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // @route GET api/users/test
 // @desc Test the users route
@@ -33,13 +35,57 @@ router.post('/register', (req, res) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if(err) throw err;
             newUser.password = hash;
-            newUser.save()
+            newUser
+              .save()
               .then(user => res.json(user))
               .catch(err => console.log(err));
           });
         });
       }
-    })
-})
+    });
+});
+
+// @route POST api/users/login
+// @desc Login User / Returning jwt token
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      // Check if user already exists
+      if(!user){
+        return res.status(404).json({ email: 'User not found' });
+      }else {
+        // Check password
+        bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if(isMatch){
+              // User password match
+              const payload = {
+                id: user.id,
+                name: user.name,
+                avatar: user.avatar
+              }
+              
+              //Sign token
+              jwt.sign(
+                payload,
+                keys.secretOrKey,
+                { expiresIn: 3600 },
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    token: 'Bearer ' + token
+                  })
+                });
+              
+            } else {
+              return res.status(400).json({ password: 'Password incorrect' });
+            }
+          })
+      }
+    });
+});
 
 module.exports = router;
